e78a9f732cf380d0f14777c287c08e59407b2014
raintangtcy <raintangtcy@gmail.com>
2014-12-17 17:10:10 +0800

    refactor packages

---
+++ nomt-master/src/main/java/org/nomt/master/mina/handler/MasterHandler.java
@@ -0,0 +1,51 @@
+/*
+ * --------------------------------------------------------------------------
+ * COPYRIGHT Ericsson Telecommunicatie B.V., 2014
+ * All rights reserved
+ * 
+ * The copyright to the computer program(s) herein is the property of
+ * Ericsson Telecommunicatie B.V.. The programs may be used and/or
+ * copied only with written permission from Ericsson Telecommunicatie
+ * B.V. or in accordance with the terms and conditions stipulated in the
+ * agreement/contract under which the program(s) have been supplied.
+ * 
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY REPRESENTATIONS OR WARRANTIES
+ * ABOUT THE SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED.
+ * ERICSSON TELECOMMUNICATIE B.V. SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
+ * THIS SOFTWARE OR ITS DERIVATIVES.
+ * --------------------------------------------------------------------------
+ */
+
+/**
+ * AgentHandler.java
+ *
+ * @author Rain Tang
+ *         Dec 16, 2014 9:44:35 PM
+ * @description
+ */
+package org.nomt.master.mina.handler;
+
+import org.apache.mina.core.service.IoHandlerAdapter;
+
+/**
+ * @author Rain Tang
+ *
+ */
+public class MasterHandler extends IoHandlerAdapter
+{
+    private final static MasterHandler instance = new MasterHandler();
+
+    private MasterHandler()
+    {
+
+    }
+
+    public static MasterHandler getInstance()
+    {
+        return instance;
+    }
+
+}
